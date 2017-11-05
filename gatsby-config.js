fb46826fd9e22e1457f06b838b8be7811eaf4a21
Hunter Chang <hunter@madwiremedia.com>
2017-11-05 16:41:06 -0700

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  siteMetadata: {
+    title: "Gatsby Starter",
+    author: "Hunter Chang",
+  },
+  pathPrefix: '/',
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/posts`,
+        name: "posts",
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/img`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          "gatsby-remark-copy-linked-files",
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `source sans pro\:300,300i,400,400i,600` // you can also specify font weights and styles
+        ]
+      }
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`
+  ],
+}
