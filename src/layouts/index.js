6d0371ffcc27bc8d85e0d8850cd7d26c15df2e9c
Hunter Chang <hunter@madwiremedia.com>
2018-03-30 11:07:29 -0600

    format indents

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,19 +3,19 @@ import Link from 'gatsby-link'
 import '../assets/scss/main.scss'
 
 class Template extends React.Component {
-  render() {
-    const { children } = this.props
+    render() {
+        const { children } = this.props
 
-    return (
-      <div>
-        {children()}
-      </div>
-    )
-  }
+        return (
+            <div>
+                {children()}
+            </div>
+        )
+    }
 }
 
 Template.propTypes = {
-  children: React.PropTypes.func
+    children: React.PropTypes.func,
 }
 
 export default Template
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,60 +3,65 @@ import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
 class BlogIndex extends React.Component {
-  render() {
-    const siteTitle = this.props.data.site.siteMetadata.title
-    const posts = this.props.data.allMarkdownRemark.edges
+    render() {
+        const siteTitle = this.props.data.site.siteMetadata.title
+        const posts = this.props.data.allMarkdownRemark.edges
 
-    return (
-      <div>
-        <Helmet title={this.props.data.site.siteMetadata.title} />
-        {posts.map(post => {
-          if (post.node.path !== '/404/') {
-            const title = post.node.frontmatter.title || post.node.path
-            return (
-              <div key={post.node.frontmatter.path}>
-                <h3>
-                  <Link to={post.node.frontmatter.path}>
-                    {post.node.frontmatter.title}
-                  </Link>
-                </h3>
-                <small>{post.node.frontmatter.date}</small>
-                <p dangerouslySetInnerHTML={{ __html: post.node.excerpt }} />
-              </div>
-            )
-          }
-        })}
-      </div>
-    )
-  }
+        return (
+            <div>
+                <Helmet title={this.props.data.site.siteMetadata.title} />
+                {posts.map(post => {
+                    if (post.node.path !== '/404/') {
+                        const title =
+                            post.node.frontmatter.title || post.node.path
+                        return (
+                            <div key={post.node.frontmatter.path}>
+                                <h3>
+                                    <Link to={post.node.frontmatter.path}>
+                                        {post.node.frontmatter.title}
+                                    </Link>
+                                </h3>
+                                <small>{post.node.frontmatter.date}</small>
+                                <p
+                                    dangerouslySetInnerHTML={{
+                                        __html: post.node.excerpt,
+                                    }}
+                                />
+                            </div>
+                        )
+                    }
+                })}
+            </div>
+        )
+    }
 }
 
 BlogIndex.propTypes = {
-  route: React.PropTypes.object,
+    route: React.PropTypes.object,
 }
 
 export default BlogIndex
 
 export const pageQuery = graphql`
-  query IndexQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          frontmatter {
-            path
-            date(formatString: "DD MMMM, YYYY")
-          }
-          frontmatter {
-            title
-          }
+    query IndexQuery {
+        site {
+            siteMetadata {
+                title
+            }
         }
-      }
+        allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+            edges {
+                node {
+                    excerpt
+                    frontmatter {
+                        path
+                        date(formatString: "DD MMMM, YYYY")
+                    }
+                    frontmatter {
+                        title
+                    }
+                }
+            }
+        }
     }
-  }
 `
